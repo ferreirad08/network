f7f9d69a97b83634a15607f414c0205bc5424118
David Ferreira <46223447+ferreirad08@users.noreply.github.com>
2021-06-17 02:16:54 -0400

    Add files via upload

---
+++ network.py
@@ -0,0 +1,46 @@
+import platform
+import os
+
+
+class Network:
+
+    __version__ = '1.0.0'
+
+    def __init__(self, remote_host: str) -> None:
+        self.remote_host: str = remote_host
+
+    def ping(self, count: int=1) -> int:
+        '''
+        Mede a latência da conexão
+        '''
+        return os.system(f'ping -c {count} {self.remote_host}')
+
+    def is_connected(self) -> bool:
+        '''
+        Verifica se o dispositivo está corretamente conectado à internet
+        '''
+        return self.ping() == 0
+
+    def get_port(self, min: int=1, max: int=100) -> str:
+        '''
+        Obtém a porta em que a conexão foi aberta
+        '''
+        fname: str = 'tempfile.txt'
+        os.system(f'nmap -oN {fname} -p {min}-{max} {self.remote_host}')
+
+        with open(fname, 'r') as f:
+            lines: list = f.readlines()
+            port, *_ = lines[-3].split('/')
+
+        os.remove(fname)
+        return port
+
+
+if __name__ == '__main__':
+
+    if platform.system() == 'Linux':
+
+        network = Network(remote_host='www.anaconda.com')
+        network.ping(count=4)
+        print('Network is connected:', network.is_connected())
+        print('Port:', network.get_port())
